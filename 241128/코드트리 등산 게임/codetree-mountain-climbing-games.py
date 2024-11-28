038bda6607227109dff1698acadc338113a4428d
gimezi <139518026+gimezi@users.noreply.github.com>
2024-11-28 18:16:39 +0900

    Title: 코드트리 등산 게임, Time: 47ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241128/코드트리 등산 게임/codetree-mountain-climbing-games.py
+++ 241128/코드트리 등산 게임/codetree-mountain-climbing-games.py
@@ -3,20 +3,23 @@ class MountainCalculator:
         self.mountains = []
         self.dp = []  # 각 위치의 LIS 길이 저장
         
-    def calculate_dp(self, start_idx):
-        tails = [0] * len(self.mountains)
+    def calculate_dp(self):
+        n = len(self.mountains)
+        self.dp = [0] * n
+        tails = [0] * n
         size = 0
         
-        # start_idx 이전까지는 이미 계산되어 있음
-        for i in range(start_idx, len(self.mountains)):
-            if i == 0:
-                tails[0] = self.mountains[i]
-                size = 1
+        for i in range(1, n):  # 1부터 시작
+            if i == 1:
+                if self.mountains[i] > self.mountains[0]:
+                    tails[size] = self.mountains[i]
+                    self.dp[i] = 1
+                    size += 1
                 continue
                 
-            if self.mountains[i] > tails[size-1]:
+            if self.mountains[i] > tails[size-1] and size > 0:
                 tails[size] = self.mountains[i]
-                self.dp[i] = size
+                self.dp[i] = size + 1
                 size += 1
             else:
                 # 이진 탐색으로 위치 찾기
@@ -28,25 +31,26 @@ class MountainCalculator:
                     else:
                         left = mid + 1
                 tails[left] = self.mountains[i]
-                self.dp[i] = left
+                self.dp[i] = left + 1
     
     def init_mountains(self, mountains):
         self.mountains = mountains
-        self.dp = [0] * len(mountains)
-        self.calculate_dp(0)
+        self.calculate_dp()
     
     def add_mountain(self, height):
         self.mountains.append(height)
-        self.dp.append(0)
-        self.calculate_dp(len(self.mountains)-1)
+        self.calculate_dp()
     
     def remove_mountain(self):
         if self.mountains:
             self.mountains.pop()
-            self.dp.pop()
+            if self.mountains:  # 산이 남아있으면 다시 계산
+                self.calculate_dp()
+            else:
+                self.dp = []
     
     def calculate(self, idx):
-        h_idx = self.dp.index(max(self.dp))
+        h_idx = self.dp.index(max(self.dp)) if max(self.dp) > 0 else 0
         print(1000000 * self.dp[idx] + 1000000 + 1000000 * max(self.dp) + self.mountains[h_idx])
 
 calculator = MountainCalculator()
