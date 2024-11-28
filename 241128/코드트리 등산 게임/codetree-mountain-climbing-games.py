bcfa354c1a598da48ef944103a47f43a3c98ec58
gimezi <139518026+gimezi@users.noreply.github.com>
2024-11-28 18:14:18 +0900

    Title: 코드트리 등산 게임, Time: 5031ms, Memory: 23MB, Status: Time Limit Exceed - CodeTree

--- 241128/코드트리 등산 게임/codetree-mountain-climbing-games.py
+++ 241128/코드트리 등산 게임/codetree-mountain-climbing-games.py
@@ -1,44 +1,45 @@
-def calculate(mountains, idx, num_of_mountains):
-    dp = [0 for _ in range(num_of_mountains)]
-    tails = [0] * num_of_mountains
-    size = 0
+class MountainCalculator:
+    def __init__(self):
+        self.mountains = []
+        self.dp = []  # 각 위치의 LIS 길이 저장
+        
+    def init_mountains(self, mountains):
+        self.mountains = mountains
+        self.dp = [0] * len(mountains)
+        # 초기 dp 계산
+        for i in range(1, len(mountains)):
+            for j in range(0, i):
+                if mountains[i] > mountains[j]:
+                    self.dp[i] = max(self.dp[i], self.dp[j] + 1)
     
-    for i in range(num_of_mountains):
-        # 이진 탐색으로 위치 찾기
-        if i == 0:
-            tails[0] = mountains[i]
-            size = 1
-            continue
-            
-        if mountains[i] > tails[size-1]:
-            tails[size] = mountains[i]
-            dp[i] = size
-            size += 1
-        else:
-            left, right = 0, size
-            while left < right:
-                mid = (left + right) // 2
-                if tails[mid] >= mountains[i]:
-                    right = mid
-                else:
-                    left = mid + 1
-            tails[left] = mountains[i]
-            dp[i] = left
+    def add_mountain(self, height):
+        self.mountains.append(height)
+        self.dp.append(0)
+        # 새로 추가된 산에 대해서만 dp 계산
+        i = len(self.mountains) - 1
+        for j in range(0, i):
+            if self.mountains[i] > self.mountains[j]:
+                self.dp[i] = max(self.dp[i], self.dp[j] + 1)
+    
+    def remove_mountain(self):
+        if self.mountains:
+            self.mountains.pop()
+            self.dp.pop()
+    
+    def calculate(self, idx):
+        h_idx = self.dp.index(max(self.dp))
+        print(1000000 * self.dp[idx] + 1000000 + 1000000 * max(self.dp) + self.mountains[h_idx])
 
-    h_idx = dp.index(max(dp))
-    print(1000000 * dp[idx] + 1000000 + 1000000 * max(dp) + mountains[h_idx])
-
+calculator = MountainCalculator()
 n = int(input())
-mountains = []
-num_of_mountains = 0
+
 for _ in range(n):
     commands = list(map(int, input().split()))
     if commands[0] == 100:
-        num_of_mountains = commands[1]
-        mountains = commands[2:]
+        calculator.init_mountains(commands[2:])
     elif commands[0] == 200:
-        mountains.append(commands[1])
+        calculator.add_mountain(commands[1])
     elif commands[0] == 300:
-        mountains.pop()
+        calculator.remove_mountain()
     else:
-        calculate(mountains, commands[1] - 1, len(mountains))
+        calculator.calculate(commands[1] - 1)
